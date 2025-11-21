eb62fec888cf92e5908c3ad029cbf212c65f0fc7
fjovanovic-da <filip.jovanovic@dataart.com>
2025-11-21 11:59:43 +0100

    feat: added routing using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { FilterProvider } from "./components/contexts/FilterContext";
 import WelcomeBoard from "./components/WelcomeBoard/WelcomeBoard";
 
@@ -6,7 +7,11 @@ function App() {
   return (
     <FilterProvider>
       <div className="App">
-        <WelcomeBoard />
+        <Routes>
+          <Route path="/" element={<WelcomeBoard />} />
+          {/* Add more routes here as needed */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
     </FilterProvider>
   );
